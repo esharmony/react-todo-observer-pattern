85c87fadc6fbce95f1d0e905e9d0cd07a7c61782
Matthew Barnden <development.esharmony@gmail.com>
2021-08-07 16:59:51 +0100

    fix tabbing issue

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -3,64 +3,64 @@ import ListItemObserver from '../Observers/Abstractions/ListObserver';
 import TodoList, { ItemState, TodoListState } from '../Subjects/TodoList';
 
 export default class Component
-	extends React.Component<{}, TodoListState>
-	implements ListItemObserver
+  extends React.Component<{}, TodoListState>
+  implements ListItemObserver
 {
-	constructor(props: {}) {
-		super(props);
+  constructor(props: {}) {
+    super(props);
 
-		this.state = {
-			ListItems: [],
-			ListSetting: ItemState.Todo,
-		};
-	}
+    this.state = {
+      ListItems: [],
+      ListSetting: ItemState.Todo,
+    };
+  }
 
-	componentDidUpdate() {
-		console.log('navigation updating');
-	}
+  componentDidUpdate() {
+    console.log('navigation updating');
+  }
 
-	componentDidMount() {
-		TodoList.registerObserver(this);
-	}
+  componentDidMount() {
+    TodoList.registerObserver(this);
+  }
 
-	updated = (state: TodoListState) => {
-		if (this.state.ListSetting !== state.ListSetting) this.setState(state);
-	};
+  updated = (state: TodoListState) => {
+    if (this.state.ListSetting !== state.ListSetting) this.setState(state);
+  };
 
-	handleClick = (
-		e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
-		state: ItemState
-	) => {
-		TodoList.setListSetting(state);
-		e.preventDefault();
-	};
+  handleClick = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    state: ItemState
+  ) => {
+    TodoList.setListSetting(state);
+    e.preventDefault();
+  };
 
-	render() {
-		return (
-			<ul id="navigation">
-				<li>
-					<a
-						href=":"
-						className={
-							this.state.ListSetting === ItemState.Todo ? 'selected' : ''
-						}
-						onClick={(e) => this.handleClick(e, ItemState.Todo)}
-					>
-						Todo
-					</a>
-				</li>
-				<li>
-					<a
-						href=":"
-						className={
-							this.state.ListSetting === ItemState.Done ? 'selected' : ''
-						}
-						onClick={(e) => this.handleClick(e, ItemState.Done)}
-					>
-						Done
-					</a>
-				</li>
-			</ul>
-		);
-	}
+  render() {
+    return (
+      <ul id="navigation">
+        <li>
+          <a
+            href=":"
+            className={
+              this.state.ListSetting === ItemState.Todo ? 'selected' : ''
+            }
+            onClick={(e) => this.handleClick(e, ItemState.Todo)}
+          >
+            Todo
+          </a>
+        </li>
+        <li>
+          <a
+            href=":"
+            className={
+              this.state.ListSetting === ItemState.Done ? 'selected' : ''
+            }
+            onClick={(e) => this.handleClick(e, ItemState.Done)}
+          >
+            Done
+          </a>
+        </li>
+      </ul>
+    );
+  }
 }
