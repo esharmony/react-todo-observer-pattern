4913ecedc2a80fad93286e60a2dd4a191d1422de
Matthew Barnden <development.esharmony@gmail.com>
2021-08-07 17:10:15 +0100

    fix tabbing issue

--- src/components/listControl.tsx
+++ src/components/listControl.tsx
@@ -2,33 +2,33 @@ import React, { useEffect, useState } from 'react';
 import TodoList, { ItemState } from '../Subjects/TodoList';
 
 const ListControl: React.FC = () => {
-	const [title, setTitle] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
 
-	useEffect(() => {
-		console.log('list control updating');
-	});
+  useEffect(() => {
+    console.log('list control updating');
+  });
 
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		if (title) {
-			TodoList.addListItem({ Title: title, State: ItemState.Todo });
-			setTitle('');
-		} else {
-			alert('Add something');
-		}
-		e.preventDefault();
-	};
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    if (title) {
+      TodoList.addListItem({ Title: title, State: ItemState.Todo });
+      setTitle('');
+    } else {
+      alert('Add something');
+    }
+    e.preventDefault();
+  };
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<input
-				type="text"
-				value={title}
-				onChange={(e) => setTitle(e.target.value)}
-				placeholder="Add item todo"
-			/>
-			<input type="submit" value="Add todo" />
-		</form>
-	);
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Add item todo"
+      />
+      <input type="submit" value="Add todo" />
+    </form>
+  );
 };
 
 export default ListControl;
