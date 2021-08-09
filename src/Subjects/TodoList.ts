d251107175c62404e82936a9a65fbde9b97f4ea4
Matthew Barnden <development.esharmony@gmail.com>
2021-08-09 15:51:06 +0100

    added asyc with loading

--- src/Subjects/TodoList.ts
+++ src/Subjects/TodoList.ts
@@ -1,79 +1,90 @@
-import ListObserver from "../Observers/Abstractions/ListObserver";
-import Subject from "./Abstractions/Subject";
+import ListObserver from '../Observers/Abstractions/ListObserver';
+import Subject from './Abstractions/Subject';
 
 export interface ListItem {
-    Title:string;
-    State:ItemState;
+  Title: string;
+  State: ItemState;
 }
 
 export enum ItemState {
-    Todo = 1,
-    Done = 2
+  Todo = 1,
+  Done = 2,
 }
 
 export interface TodoListState {
-    ListItems: ListItem[];
-    ListSetting: ItemState;
-    IsLoading?: boolean;
+  ListItems: ListItem[];
+  ListSetting: ItemState;
+  IsLoading?: boolean;
 }
 
 class TodoList implements Subject {
 
-    private TodoListState: TodoListState = { ListItems: [], ListSetting: ItemState.Todo };
-    private observers: ListObserver[];
+  private TodoListState: TodoListState = {
+    ListItems: [],
+    ListSetting: ItemState.Todo,
+  };
 
-    constructor() {
-        this.observers = [];
+  private observers: ListObserver[];
+
+  constructor() {
+    this.observers = [];
+  }
+
+  setState = (state: TodoListState) => {
+    this.TodoListState = state;
+    this.notifyObservers();
+  };
+
+  setIsLoading = (isLoading: boolean) => {
+    this.TodoListState.IsLoading = isLoading;
+    this.notifyObservers();
+  };
+
+  setListSetting = (setting: ItemState) => {
+    this.TodoListState.ListSetting = setting;
+    this.notifyObservers();
+  };
+
+  addListItems = (items: ListItem[]) => {
+    this.TodoListState.ListItems = this.TodoListState.ListItems.concat(items);
+    this.notifyObservers();
+  };
+
+  addListItem = (item: ListItem) => {
+    this.TodoListState.ListItems.push(item);
+    this.notifyObservers();
+  };
+
+  addListItemAsync = async (item: ListItem) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        this.addListItem(item);
+        resolve('done');
+      }, 3000);
+    });
+  };
+
+  changeListItemState = (itemIndex: number) => {
+    const item = this.TodoListState.ListItems[itemIndex];
+    if (item.State === ItemState.Todo) {
+      item.State = ItemState.Done;
+    } else {
+      item.State = ItemState.Todo;
     }
+    this.notifyObservers();
+  };
 
-    setState = (state: TodoListState) => {
-        this.TodoListState = state;
-        this.notifyObservers();
-    }
+  registerObserver = (o: ListObserver): void => {
+    this.observers.push(o);
+  };
 
-    setIsLoading = (isLoading:boolean) => {
-        this.TodoListState.IsLoading = isLoading;
-        this.notifyObservers();
-    }
+  removeObserver = (o: ListObserver): void => {
+    this.observers.splice(this.observers.indexOf(o), 1);
+  };
 
-    setListSetting = (setting:ItemState) => {
-        this.TodoListState.ListSetting = setting;
-        this.notifyObservers();
-    }
-
-    addListItems = (items: ListItem[]) => {
-        this.TodoListState.ListItems = this.TodoListState.ListItems.concat(items);
-        this.notifyObservers();
-    }
-
-    addListItem = (item: ListItem) => {
-        this.TodoListState.ListItems.push(item);
-        this.notifyObservers();
-    }
-
-    changeListItemState = (itemIndex: number) => {
-        const item = this.TodoListState.ListItems[itemIndex];
-        if(item.State === ItemState.Todo) {
-            item.State = ItemState.Done;
-        }
-        else {
-            item.State = ItemState.Todo;
-        }
-        this.notifyObservers();
-    }
-
-    registerObserver = (o: ListObserver):void => {
-        this.observers.push(o);
-    }
-
-    removeObserver = (o: ListObserver):void => {
-        this.observers.splice(this.observers.indexOf(o), 1);
-    };
-
-    notifyObservers = ():void => {
-        this.observers.forEach(observer => observer.updated(this.TodoListState))
-    };
-
+  notifyObservers = (): void => {
+    this.observers.forEach((observer) => observer.updated(this.TodoListState));
+  };
 }
 
 // export an instance - use the single instance accross the app
--- src/components/listControl.tsx
+++ src/components/listControl.tsx
@@ -1,32 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+
 import TodoList, { ItemState } from '../Subjects/TodoList';
 
-const ListControl: React.FC = () => {
+
+const ListControl = () => {
   const [title, setTitle] = useState<string>('');
+	const [asyncRequested, setAsyncRequested] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    console.log('list control updating');
-  });
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    if (title) {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+    if (title && !asyncRequested) {
       TodoList.addListItem({ Title: title, State: ItemState.Todo });
       setTitle('');
-    } else {
+    } 
+		else if(title && asyncRequested){
+      setIsLoading(true);
+      setTitle('loading..');
+			await TodoList.addListItemAsync({Title:title, State: ItemState.Todo});
+      setIsLoading(false);
+			setTitle('');
+		}
+		else {
       alert('Add something');
     }
-    e.preventDefault();
   };
 
+	const handleAsyncOption = () => {
+		setAsyncRequested(asyncRequested !== true);
+	}
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={(e) => handleSubmit(e)}>
       <input
         type="text"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
         placeholder="Add item todo"
       />
-      <input type="submit" value="Add todo" />
+			<input type="submit" value="Add todo" disabled={isLoading} />
+			<label id="asyncOption"> do async <input type="checkbox" onClick={handleAsyncOption} /></label>
     </form>
   );
 };
